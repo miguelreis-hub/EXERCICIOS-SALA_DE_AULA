2639c3933062832715924afd3c613b58bd19541c
miguelreis-hub <miguel.reis@ba.estudante.senai.br>
2026-05-20 16:30:24 -0300

    Add files via upload

---
+++ atv copy 6.js
@@ -0,0 +1,40 @@
+
+const vetor_nomes = ['miguel','joão','Bruno',"Luis"]
+
+console.log("Exibindo mensagem.......")
+console.log("")
+
+
+vetor_nomes.push("Juninho viadinho")
+
+console.log(vetor_nomes)
+
+console.log("")
+
+
+vetor_nomes.unshift("Miguelzão")
+
+console.log("")
+
+console.log(vetor_nomes)
+console.log("")
+
+console.log("Removendo dados ......")
+
+console.log("")
+
+vetor_nomes.shift()
+
+console.log(vetor_nomes)
+
+console.log("")
+
+console.log("Removendo ultimo indice do vetor..........")
+console.log("")
+
+vetor_nomes.pop()
+
+console.log(vetor_nomes)
+
+
+
